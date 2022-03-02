f12b1c2f772d14ce653c377548ec4df52472bf55
Leo Gutierrez <Leonardo.Gutierrez@autozone.com>
2022-03-01 23:41:33 -0700

    add demo project showing generics

---
+++ generics-intro/go.mod
@@ -0,0 +1,3 @@
+module generics-intro
+
+go 1.18
---
+++ generics-intro/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func Print[T any](s []T) {
+	for _, v := range s {
+		fmt.Print(v, " ")
+	}
+
+	fmt.Println()
+}
+
+func main() {
+	ints := []int{1, 2, 3}
+	texts := []string{"one", "two", "three"}
+	Print(ints)
+	Print(texts)
+}
