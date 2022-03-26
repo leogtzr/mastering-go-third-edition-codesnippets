0dcdfc985a6295d02d24aefff3b8818d9ba075f5
leogtzr <leogutierrezramirez@gmail.com>
2022-03-25 22:44:22 -0700

    add example code for the constant generator iota

---
+++ iota1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+)
+
+type (
+    Digit int
+    Power2 int
+)
+
+const PI = 3.1415926
+
+const (
+    C1 = "C1C1C1"
+    C2 = "C2C2C2"
+    C3 = "C3C3C3"
+)
+
+func main() {
+    fmt.Println(C1)
+    fmt.Println(PI)
+}
